2cedfbb44bd80477f766bafc3243157c1721f9c1
Sandro81 <sandro.fioravanti81@gmail.com>
2019-08-25 11:30:23 +0100

    part2

--- src/app/section11/l124/servers/server/server.component.ts
+++ src/app/section11/l124/servers/server/server.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ServersService} from '../servers.service';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -10,10 +11,19 @@ export class ServerComponent implements OnInit {
 
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    // the + is useful to pass a number and not a string
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.server = this.serversService.getServer(+params['id']);
+        }
+      );
   }
 
 }
